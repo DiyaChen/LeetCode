4470499f9f01265322a21d32b2eebb593bd2b877
DiyaChen <daisychen0909@gmail.com>
2015-11-11 17:03:30 -0500

    revision

--- 190 Reverse Bits.java
+++ 190 Reverse Bits.java
@@ -12,17 +12,14 @@ Related problem: Reverse Integer
 */
 
 public class Solution {
-    public int reverseBits(int n) {	// you need treat n as an unsigned value
-        int result = 0;
-        for(int i = 0; i < 32; i++){
-            if((n & 1) == 1){
-                result = result << 1;
-                result = result + 1;
-            }else{
-                result = result << 1;
-            }
-            n = n >> 1;
+    public int reverseBits(int n) {
+        int res = 0;
+       for(int i = 0; i< 32; i++)
+        {
+            res <<= 1;
+            res += (n & 1);
+            n >>= 1;
         }
-        return result;
+       return res; 
     }
 }
--- 201 Bitwise AND of Numbers Range.java
+++ 201 Bitwise AND of Numbers Range.java
@@ -6,13 +6,16 @@ Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of 
 For example, given the range [5, 7], you should return 4.
 */
 
-//[26, 30]: 11010　　11011　　11100　　11101　　11110 => 1100
+//[26, 30]: 11010　　11011　　11100　　11101　　11110 => 11000
 public class Solution {
-    public int rangeBitwiseAnd(int m, int n) {
-        int mask = Integer.MAX_VALUE;
-        while((m & mask) != (n & mask)){
-            mask <<= 1;
+    public int rangeBitwiseAnd(int m, int n) {//two neighboring number are different in the last bit, the last bit must be 0 after &. find the left leader that is the same for m and n
+        int count = 0;
+        while (m != n)
+        {
+            m = m >> 1;
+            n = n >> 1;
+            count++;
         }
-        return m & mask;
+         return n << count;   
     }
 }
